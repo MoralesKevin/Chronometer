f7e428ed42f07e86922f4b9fd1622c5740ee04cc
MoralesKevin <kevinmorales@live.fr>
2019-01-16 03:57:04 +0100

    Updates

---
+++ precache-manifest.98c932b55da4ebf2908f8d736991fc39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d7e50ec578c583c8cbd",
+    "url": "/chronometer/static/js/runtime~main.4d7e50ec.js"
+  },
+  {
+    "revision": "4553a9a1708f55ace316",
+    "url": "/chronometer/static/js/main.4553a9a1.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/chronometer/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "4553a9a1708f55ace316",
+    "url": "/chronometer/static/css/main.2d5cbac8.chunk.css"
+  },
+  {
+    "revision": "9f91e93a94b13a977a3e455a605a81f0",
+    "url": "/chronometer/index.html"
+  }
+];
